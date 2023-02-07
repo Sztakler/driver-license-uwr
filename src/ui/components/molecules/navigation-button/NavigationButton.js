9b303c3a228fc1bde15b1e4fee596d5eb376d4b4
Sztakler <krystian.jasionek@gmail.com>
2023-02-07 19:07:46 +0100

    Refactored conditional rendering of NavigationButtons and NavigationLogo

--- src/ui/components/molecules/navigation-button/NavigationButton.js
+++ src/ui/components/molecules/navigation-button/NavigationButton.js
@@ -4,15 +4,18 @@ import styles from "./styles.js";
 
 
 export default function NavigationButton(props) {
+  
+  let displayedComponent = <Text>Provide text or image path in props</Text>;
+  
+  if (props.text) {
+    displayedComponent = <Text style={styles.text}>{props.text}</Text>;
+  } else if (props.image) {
+    displayedComponent = <Image style={styles.image} source={props.image} />;
+  }
+
   return (
     <View style={styles.container}>
-      {props.text !== undefined ? (
-        <Text style={styles.text}>{props.text}</Text>
-      ) : props.image !== undefined ? (
-        <Image style={styles.image} source={props.image} />
-      ) : (
-        <Text>Provide text or image path in props</Text>
-      )}
+     {displayedComponent}
     </View>
   );
 }
