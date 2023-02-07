9ee48ebfb5e97bbe839bd4021ce8dfd5090d47ae
Sztakler <krystian.jasionek@gmail.com>
2023-02-07 23:22:36 +0100

    Began implementation of navigation

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
-import { StyleSheet, View } from 'react-native';
-import HomePage from './src/ui/pages/HomePage';
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, View } from "react-native";
+import HomePage from "./src/ui/pages/HomePage";
 
 export default function App() {
   return (
-    <View style={styles.body}>
-      <HomePage></HomePage>
-    </View>
+    <NavigationContainer>
+      <View style={styles.body}>
+        <HomePage></HomePage>
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   body: {
     height: "100%",
-  }
+  },
 });
