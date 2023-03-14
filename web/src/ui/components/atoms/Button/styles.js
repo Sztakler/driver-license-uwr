15486e4d86f965b772004a6d956d6a0d13bb53e4
supreme229 <bjanikowski6@gmail.com>
2023-03-14 20:06:50 +0100

    Further updates in FAQ

--- web/src/ui/components/atoms/Button/styles.js
+++ web/src/ui/components/atoms/Button/styles.js
@@ -3,7 +3,12 @@ const buttonDefaultClasses = "py-3 px-5";
 const buttonPrimaryClasses =
 	"border-2 border-solid border-gray-500 bg-black text-white rounded-full";
 
-const buttonNavbarClasses = "block ";
+const buttonNavbarClasses = "block";
+
+const buttonUnderscoredClasses =
+	"text-blue-600 underline underline-offset-4 hover:text-blue-400";
+
+const buttonBoldClasses = "font-bold";
 
 const buttonActiveClasses = "border-b-4 border-solid border-black";
 
@@ -13,6 +18,8 @@ export {
 	buttonDefaultClasses,
 	buttonPrimaryClasses,
 	buttonNavbarClasses,
+	buttonUnderscoredClasses,
+	buttonBoldClasses,
 	buttonActiveClasses,
 	buttonHighlightedClasses,
 };
