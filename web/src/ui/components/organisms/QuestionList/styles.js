c182d64bbfec4f70e68cb5d23e9013e798cd0b72
supreme229 <bartek.janikowski@wp.pl>
2023-03-17 19:45:22 +0100

    Added FAQ again

---
+++ web/src/ui/components/organisms/QuestionList/styles.js
@@ -0,0 +1,59 @@
+import tw from "tailwind-styled-components";
+
+const QuestionListWrapper = tw.div`
+	flex 	
+	self-center
+	flex-col 
+`;
+
+const QuickActions = tw.div``;
+
+const ListContainer = tw.div`
+	border-4
+	rounded-xl
+	border-gray-300
+	bg-gray-100
+	[&>:not(:last-child)]:border-b-[0.2rem]
+	[&>:not(:last-child)]:border-gray-400
+`;
+
+const ListItem = tw.div`
+	w-[55rem]
+	p-5
+`;
+
+const Question = tw.h4`
+`;
+
+const Answer = tw.p`
+	
+`;
+
+const QuestionSection = tw.div`
+	flex
+	flex-row
+	items-center
+	gap-2
+`;
+
+const AnswerSection = tw.div`
+	pl-8
+	pr-5
+`;
+
+const Arrow = tw.span`
+	material-symbols-outlined
+	cursor-pointer
+`;
+
+export {
+	QuestionListWrapper,
+	QuickActions,
+	ListContainer,
+	ListItem,
+	Question,
+	Answer,
+	QuestionSection,
+	AnswerSection,
+	Arrow,
+};
