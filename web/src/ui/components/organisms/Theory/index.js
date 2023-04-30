5de378e0a492e50c7a772792ff7b1dc7fd08e5bb
Krystian Jasionek <krystian.jasionek@gmail.com>
2023-04-30 14:34:20 +0200

    Added illustration for Theory page. Added icons.

--- web/src/ui/components/organisms/Theory/index.js
+++ web/src/ui/components/organisms/Theory/index.js
@@ -180,107 +180,57 @@ export default function Theory({ navigation, content }) {
 	};
 
 	return (
-		<Container className="relative">
-			{isSidebarVisible ? (
-				<SidebarContainer>
-					<button
-						className="absolute top-[16px] left-[330px] flex flex-col cursor-pointer"
-						onClick={handleSidebarClick}
-					>
-						<svg
-							width="29"
-							height="22"
-							viewBox="0 0 29 22"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M10.5141 1L1 10.5141L10.5141 20.0282"
-								stroke="#292D32"
-								stroke-width="2"
-								stroke-miterlimit="10"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-							<path
-								d="M27.6454 10.5144H1.26611"
-								stroke="#292D32"
-								stroke-width="2"
-								stroke-miterlimit="10"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-						</svg>
-						<p className="rotate-90 translate-y-16 -translate-x-10">
-							Zwiń spis treści
-						</p>
-						<input
-							type="checkbox"
-							className="appearance-none w-[60px] checked:bg-blue-500"
-						></input>
+		<Container>
+			<HeaderContainer>
+				<TitleContainer>
+					<Title className="text-8xl">Podręcznik</Title>
+					<Subtitle className="text-2xl pt-5 max-w-prose">Ten darmowy podręcznik pozwoli ci szybko nauczyć się podstaw teorii zasad ruchu drogowego.</Subtitle>
+				</TitleContainer>
+				<IllustrationContainer>
+					<Image src={TextbookImages.MainIllustration}></Image>
+					<button className="self-center mt-10" onClick={scrollDown}>
+						<Image src={Arrows.Down}></Image>
 					</button>
-					<Sidebar navigation={navigation}></Sidebar>
-				</SidebarContainer>
-			) : (
-				<button
-					className="absolute top-[16px] left-[8px] flex flex-col cursor-pointer"
-					onClick={handleSidebarClick}
-				>
-					<svg
-						className="rotate-180"
-						width="29"
-						height="22"
-						viewBox="0 0 29 22"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							d="M10.5141 1L1 10.5141L10.5141 20.0282"
-							stroke="#292D32"
-							stroke-width="2"
-							stroke-miterlimit="10"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						/>
-						<path
-							d="M27.6454 10.5144H1.26611"
-							stroke="#292D32"
-							stroke-width="2"
-							stroke-miterlimit="10"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						/>
-					</svg>
-					<p className="rotate-90 translate-y-16 -translate-x-14">
-						Rozwiń spis treści
-					</p>
-					<input
-						type="checkbox"
-						className="appearance-none w-[60px] checked:bg-blue-500"
-					></input>
-				</button>
-			)}
-			<Content>
-				<Title>Znaki ostrzegawcze</Title>
-				<Subtitle>rozdział: Znaki pionowe</Subtitle>
-				<Paragraph style="text-[#0d0d0d] max-w-prose text-lg text-left">
-					Znaki te wskazują kierującemu jak powinien się zachować w miejscu, do
-					którego się zbliża. Znaki nakazujące kierunek jazdy mogą być
-					umieszczone na przedłużeniu osi jezdni (drogi) lub na samej jezdni
-					(drodze).
-				</Paragraph>
-				<PanelsScroller>
-					{panelsContents.map((panel) => (
-						<Panel>
-							<Image
-								src={panel.image}
-								className="h-[14rem] text-elipsis"
-							></Image>
-							<Paragraph style="text-[#0d0d0d]">{panel.description}</Paragraph>
-						</Panel>
-					))}
-				</PanelsScroller>
-			</Content>
+				</IllustrationContainer>
+			</HeaderContainer>
+			<MainContainer id="MainContainer" className="relative w-full">
+				{isSidebarVisible ?
+					<SidebarContainer>
+						<button className="absolute top-[16px] left-[330px] flex flex-col cursor-pointer" onClick={handleSidebarClick}>
+							<svg width="29" height="22" viewBox="0 0 29 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M10.5141 1L1 10.5141L10.5141 20.0282" stroke="#292D32" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+								<path d="M27.6454 10.5144H1.26611" stroke="#292D32" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+							</svg>
+							<p className="rotate-90 translate-y-16 -translate-x-10">Zwiń spis treści</p>
+							<input type="checkbox" className="appearance-none w-[60px] checked:bg-blue-500">
+							</input>
+						</button>
+						<Sidebar navigation={navigation}></Sidebar>
+					</SidebarContainer>
+					:
+					<button className="absolute top-[16px] left-[8px] flex flex-col cursor-pointer" onClick={handleSidebarClick}>
+						<svg className="rotate-180" width="29" height="22" viewBox="0 0 29 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M10.5141 1L1 10.5141L10.5141 20.0282" stroke="#292D32" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+							<path d="M27.6454 10.5144H1.26611" stroke="#292D32" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+						</svg>
+						<p className="rotate-90 translate-y-16 -translate-x-14">Rozwiń spis treści</p>
+						<input type="checkbox" className="appearance-none w-[60px] checked:bg-blue-500">
+						</input>
+					</button>}
+				<Content>
+					<Title>Znaki ostrzegawcze</Title>
+					<Subtitle>rozdział: Znaki pionowe</Subtitle>
+					<Paragraph style="text-[#0d0d0d] max-w-prose text-lg text-left" content="Znaki te wskazują kierującemu jak powinien się zachować w miejscu, do którego się zbliża. Znaki nakazujące kierunek jazdy mogą być umieszczone na przedłużeniu osi jezdni (drogi) lub na samej jezdni (drodze)."></Paragraph>
+					<PanelsScroller>
+						{panelsContents.map(panel =>
+							<Panel>
+								<Image src={panel.image} className="h-[14rem] text-elipsis"></Image>
+								<Paragraph style="text-[#0d0d0d]" content={panel.description}></Paragraph>
+							</Panel>
+						)}
+					</PanelsScroller>
+				</Content>
+			</MainContainer>
 		</Container>
 	);
 }
