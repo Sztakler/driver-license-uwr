6897391bc81e9cfdf2c50a059f50f8402d901837
Krystian Jasionek <krystian.jasionek@gmail.com>
2023-05-06 14:58:11 +0200

    Modified script converting database to JSON (array of answers, correct answer is only an index).

--- web/src/data/convert-to-json.py
+++ web/src/data/convert-to-json.py
@@ -1,16 +1,28 @@
 import json
 import csv
 
+def get_answers(entry):
+    if entry[8] == "SPECJALISTYCZNY":
+        return entry[3:6]
+    else:
+        return ["Tak", "Nie"]
+
+def get_correct_answers_index(entry):
+    correct_answer = entry[6]
+    if correct_answer in "AT" :
+        return 0
+    elif correct_answer in "BN":
+        return 1
+    else:
+        return 2
 
 def create_entry_dict(entry):
     entry_dict = {
         "nazwa_pytania": entry[0],
         "numer_pytania": entry[1],
         "pytanie": entry[2],
-        "odpowiedz_a": entry[3],
-        "odpowiedz_b": entry[4],
-        "odpowiedz_c": entry[5],
-        "poprawna_odpowiedz": entry[6],
+        "odpowiedzi": get_answers(entry),
+        "poprawna_odpowiedz": get_correct_answers_index(entry),
         "media": entry[7],
         "zakres_struktury": entry[8],
         "liczba_punktow": entry[9],
@@ -38,7 +50,10 @@ for i in range(len(data)):
     # entry_dict.append(entry_dict(entry))
     data_dict[i] = create_entry_dict(data[i])
 
-print(data_dict)
+# print(json.loads(str(data_dict).replace("'", '"')))
+formatted_data = json.dumps(data_dict, indent=2)
+
+print(formatted_data)
 # for key, value in data_dict.items():
 #     print(f"{key}: {value}")
 #     print()
