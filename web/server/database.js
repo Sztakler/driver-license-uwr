3eecbbcec978a50c7f1e631327b6c37caa581018
Krystian Jasionek <krystian.jasionek@gmail.com>
2023-05-22 20:28:43 +0200

    Changed database host to neondb.

--- web/server/database.js
+++ web/server/database.js
@@ -1,11 +1,18 @@
 const Pool = require("pg").Pool;
+require('dotenv').config();
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=endpoint%3D${ENDPOINT_ID}`;
 
 const pool = new Pool({
-	user: "postgres",
-	password: "drliuw23",
-	host: "localhost",
+	user: PGUSER,
+	password: PGPASSWORD,
+  host: PGHOST, 
 	port: 5432,
-	database: "driver_license_uwr",
+	database: PGDATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
