6bd4f7b4e8975e2d22b3e47c89fee99b295e39b1
supreme229 <bartek.janikowski@wp.pl>
2023-05-23 19:55:26 +0200

    Improved backende queries in practice and image handling

--- web/server/database.sql
+++ web/server/database.sql
@@ -1,31 +1,33 @@
 create TABLE questions(
-  id serial primary key,
-  pytanie varchar(400),
-  odpowiedzi varchar(400) [],
-  poprawna_odpowiedz integer,
-  media bytea,
-  liczba_punktow integer,
-  zakres_struktury varchar(50),
+	id serial primary key,
+	pytanie varchar(400),
+	odpowiedzi varchar(400) [],
+	poprawna_odpowiedz integer,
+	media varchar(255),
+	liczba_punktow integer,
+	zakres_struktury varchar(50),
 )
 insert into questions (
-  "pytanie",
-  "odpowiedzi",
-  "poprawna_odpowiedz",
-  "liczba_punktow",
-  "zakres_struktury"
-)
-values 
-(
-  'Czy w przedstawionej sytuacji masz prawo - mimo podawanego sygnału - skręcić w prawo?',
-  Array['Tak', 'Nie'],
-  0,
-  3,
-  'PODSTAWOWY'
-),
-(
-  'Jaką odległość należy zachować między jadącymi kolumnami samochodów ciężarowych?',
-  Array['Nie mniejszą niż 800 metrów', 'Nie mniejszą niż 500 metrów', 'Nie mniejszą niż 200 metrów'],
-  1,
-  3,
-  'SPECJALISTYCZNY'
-);
+		"pytanie",
+		"odpowiedzi",
+		"poprawna_odpowiedz",
+		"media",
+		"liczba_punktow",
+		"zakres_struktury"
+	)
+values (
+		'Czy w przedstawionej sytuacji masz prawo - mimo podawanego sygnału - skręcić w prawo?',
+		Array ['Tak', 'Nie'],
+		0,
+		'https://exam-media.s3.eu-north-1.amazonaws.com/test.jpg',
+		3,
+		'PODSTAWOWY'
+	),
+	(
+		'Jaką odległość należy zachować między jadącymi kolumnami samochodów ciężarowych?',
+		Array ['Nie mniejszą niż 800 metrów', 'Nie mniejszą niż 500 metrów', 'Nie mniejszą niż 200 metrów'],
+		1,
+		'https://exam-media.s3.eu-north-1.amazonaws.com/test.jpg',
+		3,
+		'SPECJALISTYCZNY'
+	);
