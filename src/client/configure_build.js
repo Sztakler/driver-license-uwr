fb15fcda8672b5d5976b01db27ab49689a23f32d
supreme229 <bartek.janikowski@wp.pl>
2023-09-01 03:11:59 +0200

    README update

--- src/client/configure_build.js
+++ src/client/configure_build.js
@@ -1,3 +1,3 @@
-export const urlToServer = "https://51.20.5.42";
+export const urlToServer = "http://localhost:4000";
 // https://51.20.5.42 - Amazon EC2 server (not working with local app, only gh-pages)
 // http://localhost:4000 - local
